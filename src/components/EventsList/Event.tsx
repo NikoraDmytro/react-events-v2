3861f05a0b55e96623c0fed03b9000b79829ed4b
Dmytro <dimaniko04@ukr.net>
2021-10-09 18:08:27 +0300

    Created some mixins. Inputs of the events list now enabled only after pressing edit button.

--- src/components/EventsList/Event.tsx
+++ src/components/EventsList/Event.tsx
@@ -1,19 +1,33 @@
-import React from "react";
-import styles from "./Events.module.scss";
+import React, { useRef, useState } from "react";
+import styles from "./Event.module.scss";
 
 interface Props {
   index: number;
 }
 
 export const Event = ({ index }: Props) => {
+  const liElement = useRef<HTMLLIElement>(null);
+  const [editMode, setEditMode] = useState(false);
+
+  const toEditMode = () => setEditMode(true);
+  const editEvent = () => setEditMode(false);
+  const handleBlur = (e: React.FocusEvent<HTMLLIElement>) => {
+    if (editMode && liElement.current && !liElement.current.contains(e.relatedTarget)) {
+      editEvent();
+    }
+  }
+
   return (
-    <li className={styles.event}>
+    <li key={index} onBlur={(e) => handleBlur(e)} tabIndex={0} ref={liElement} className={styles.event}>
       <span>Custom {index}</span>
-      <input type="date" defaultValue="2019-05-22" />
-      <input type="time" defaultValue="09:00" />
-      <input type="time" defaultValue="12:00" />
-      <img src="img/edit.png" alt="Edit" />
-      <img src="img/close.png" alt="Delete" />
+      <input disabled={!editMode} type="date" defaultValue="2019-05-22" />
+      <input disabled={!editMode} type="time" defaultValue="09:00" />
+      <input disabled={!editMode} type="time" defaultValue="12:00" />
+      {editMode ?
+        <img onClick={editEvent} src={"./img/tick.png"} alt="Confirm" /> :
+        <img onClick={toEditMode} src="img/edit.png" alt="Edit" />
+      }
+      <div className={styles.delete}><img src="img/close.png" alt="Delete" /></div>
     </li>
   );
 };
--- src/components/EventsList/EventsList.tsx
+++ src/components/EventsList/EventsList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Event } from "./Event";
-import styles from "./Events.module.scss";
+import styles from "./EventsList.module.scss";
 
 const DailyEvents = () => {
   const dailyEvents = [];
