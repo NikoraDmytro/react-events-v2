13029caa7f34e2ba3e5aed6740bd067069a88722
Dmytro <dimaniko04@ukr.net>
2021-10-09 13:15:32 +0300

    Removed all the unnecessary stuff. Created events layout with simple styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.scss";
 
 function App() {
+  const renderEvent = (number: number) => (
+    <li>
+      <span>Custom {number}</span>
+      <input type="date" defaultValue="22.05.2019" />
+      <input type="time" defaultValue="09:00" />
+      <input type="time" defaultValue="12:00" />
+      <img src="img/edit.png" alt="Edit" />
+      <img src="img/close.png" alt="Delete" />
+    </li>
+  );
+
+  const renderEventsForDay = () => {
+    const eventsForDay = [];
+
+    for (let i = 0; i < 6; i++) {
+      eventsForDay.push(renderEvent(i + 1));
+    }
+
+    return (
+      <li className={styles.dailyEvents}>
+        <p className={styles.eventsDate}>22.05.2019</p>
+        <ul className={styles.EventsForDayList}>{eventsForDay}</ul>
+      </li>
+    );
+  };
+
+  const renderDailyEvents = () => {
+    const dailyEvents = [];
+
+    for (let i = 0; i < 3; i++) {
+      dailyEvents.push(renderEventsForDay());
+    }
+
+    return <ul className={styles.dailyEventsList}>{dailyEvents}</ul>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{renderDailyEvents()}</div>
     </div>
   );
 }
