a0be62a14f87fa28acd7bda3dcd097f4ff53c56a
Dmytro <dimaniko04@ukr.net>
2021-10-15 17:00:11 +0300

    Created hook for evaluating how many items fit into the container

---
+++ src/utils/useCapacity.ts
@@ -0,0 +1,33 @@
+import { RefObject, useLayoutEffect, useState } from "react";
+
+const getMargin = (child: Element): number => {
+  const style = window.getComputedStyle(child);
+  const margin = style.marginRight.slice(0, -2); //removing "px"
+
+  return Number(margin);
+};
+
+export const useCapacity = (ref: RefObject<HTMLUListElement>) => {
+  const [capacity, setCapacity] = useState(1);
+
+  useLayoutEffect(() => {
+    const getCapacity = () => {
+      if (!ref.current) return;
+
+      const child = ref.current.children[0];
+      const margin = getMargin(child);
+      const childWidth = child.clientWidth + margin;
+      const width = ref.current.offsetWidth - margin;
+
+      console.log(width, childWidth);
+
+      setCapacity(Math.floor(width / childWidth));
+    };
+
+    window.addEventListener("resize", getCapacity);
+    getCapacity();
+    return () => window.removeEventListener("resize", getCapacity);
+  }, [ref]);
+
+  return capacity;
+};
